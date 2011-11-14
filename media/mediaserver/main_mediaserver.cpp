9c5a1563377cd3d2e5ed5183ef0ae4b81ef3b32c
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-11-14 16:45:40 +0100

    boot lock: don't wait audio policy service

--- media/mediaserver/main_mediaserver.cpp
+++ media/mediaserver/main_mediaserver.cpp
@@ -60,7 +60,7 @@ int main(int argc, char** argv)
     MediaPlayerService::instantiate();
     waitBeforeAdding( String16("media.camera") );
     CameraService::instantiate();
-    waitBeforeAdding( String16("media.audio_policy") );
+    //waitBeforeAdding( String16("media.audio_policy") );
     AudioPolicyService::instantiate();
     ProcessState::self()->startThreadPool();
     IPCThreadState::self()->joinThreadPool();
