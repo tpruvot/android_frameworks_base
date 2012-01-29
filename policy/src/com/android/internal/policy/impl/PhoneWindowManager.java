5bcdd7e2ea96f843a54dbb826ba00f71e68c351c
Flemmard <flemmard@gmail.com>
2012-01-29 08:31:45 -0500

    Added CombinedBar Auto Hide (3/2) (FIX)
    This fix shows the bar again when touching screen, as it does on GN.
    
    PatchSet 2 fixes crash on some fullscreen apps.
    
    Change-Id: I8fbbbddd551ee2c053efe1cd6e0666f932200e93

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2657,7 +2657,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     public int finishAnimationLw() {
         int changes = 0;
         boolean topIsFullscreen = false;
-
         final WindowManager.LayoutParams lp = (mTopFullscreenOpaqueWindowState != null)
                 ? mTopFullscreenOpaqueWindowState.getAttrs()
                 : null;
@@ -2682,7 +2681,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 // case though.
                 if (topIsFullscreen) {
                     if (mStatusBarCanHide ||
-                        (((updateSystemUiVisibilityLw() & View.SYSTEM_UI_FLAG_LOW_PROFILE) == 1) &&
+                        (((mFocusedWindow != null) && (mFocusedWindow.getSystemUiVisibility() & View.SYSTEM_UI_FLAG_LOW_PROFILE) == 1) &&
                          (Settings.System.getInt(mContext.getContentResolver(),
                                                  Settings.System.COMBINED_BAR_AUTO_HIDE, 0) == 1))) {
                         if (DEBUG_LAYOUT) Log.v(TAG, "** HIDING status bar");
@@ -2697,7 +2696,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                                 }
                             }});
                         }
-                    } else if (DEBUG_LAYOUT) {
+                    }
+                    else if (((mFocusedWindow != null) && (mFocusedWindow.getSystemUiVisibility() & View.SYSTEM_UI_FLAG_LOW_PROFILE) == 0) &&
+                             (Settings.System.getInt(mContext.getContentResolver(),
+                                                     Settings.System.COMBINED_BAR_AUTO_HIDE, 0) == 1)) {
+                        if (mStatusBar.showLw(true)) changes |= FINISH_LAYOUT_REDO_LAYOUT;
+                    }
+                    else if (DEBUG_LAYOUT) {
                         Log.v(TAG, "Preventing status bar from hiding by policy");
                     }
                 } else {
