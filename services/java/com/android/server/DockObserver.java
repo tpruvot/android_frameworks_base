85e2f7f262e93f6c71356d7d840678ddcdfcf470
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-02-06 10:43:46 +0100

    Revert "adb dock: patchset 2"
    This reverts commit 2d6e7cda0061bb3fd7472a5f817c179aae67a67b.

--- services/java/com/android/server/DockObserver.java
+++ services/java/com/android/server/DockObserver.java
@@ -173,6 +173,7 @@ class DockObserver extends UEventObserver {
 
                             if (whichSound != null) {
                                 final String soundPath = Settings.System.getString(cr, whichSound);
+                                Slog.i(TAG, "Will play dock sound " + soundPath);
                                 if (soundPath != null) {
                                     final Uri soundUri = Uri.parse("file://" + soundPath);
                                     if (soundUri != null) {
@@ -186,12 +187,14 @@ class DockObserver extends UEventObserver {
                             }
                         }
 
-                        if (Settings.System.getInt(cr, Settings.System.DOCK_ADB_NET_ENABLED, 0) == 1)
+                        if (Settings.System.getInt(cr, Settings.System.DOCK_ADB_NET_ENABLED, 1) == 1)
                         {
                             boolean adbEnabled = (Settings.Secure.getInt(cr,
                                                          Settings.Secure.ADB_ENABLED, 0) != 0);
 
                             // both ADB and Dock setting must be active to allow the adb network switch
+                            adbEnabled = adbEnabled & (Settings.System.getInt(cr,
+                                                         Settings.System.DOCK_ADB_NET_ENABLED, 0) != 0);
 
                             Slog.i(TAG, "DockChange: adb enabled="+adbEnabled);
                             if (adbEnabled) {
