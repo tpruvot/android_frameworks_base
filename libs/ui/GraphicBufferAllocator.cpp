8f430d8e3962e87faf177a7f552023829ccb795a
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-03-01 11:01:27 +0100

    TI Video Decoder use pixel format 27 (0x1B), not handled...
    Change-Id: I7af8f23eb03bd33bc6a8a3f0bc451096d28c4351

--- libs/ui/GraphicBufferAllocator.cpp
+++ libs/ui/GraphicBufferAllocator.cpp
@@ -97,21 +97,24 @@ status_t GraphicBufferAllocator::alloc(uint32_t w, uint32_t h, PixelFormat forma
         w = h = 1;
 
     // we have a h/w allocator and h/w buffer is requested
-    status_t err; 
+    status_t err;
 
 #ifdef MISSING_EGL_PIXEL_FORMAT_YV12
     if (format == HAL_PIXEL_FORMAT_YV12) {
-	format = HAL_PIXEL_FORMAT_RGBX_8888;
+        format = HAL_PIXEL_FORMAT_RGBA_8888;
+    }
+    if (format == 0x1B) {
+        format = HAL_PIXEL_FORMAT_RGBA_8888;
     }
     if (usage & GRALLOC_USAGE_EXTERNAL_DISP) {
-	usage ^= GRALLOC_USAGE_EXTERNAL_DISP;
+        usage ^= GRALLOC_USAGE_EXTERNAL_DISP;
     }
 #endif
     err = mAllocDev->alloc(mAllocDev, w, h, format, usage, handle, stride);
 
     LOGW_IF(err, "alloc(%u, %u, %d, %08x, ...) failed %d (%s)",
             w, h, format, usage, err, strerror(-err));
-    
+
     if (err == NO_ERROR) {
         Mutex::Autolock _l(sLock);
         KeyedVector<buffer_handle_t, alloc_rec_t>& list(sAllocList);
