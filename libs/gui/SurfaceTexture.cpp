f9edeaf2d92cd202a5dd4a0654c98202235dc180
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-05-04 08:33:52 +0200

    Revert "Update SurfaceTexture interface for forward compatibility"
    This reverts commit 3211e36679dbc8f0016d0e05922001ffc31241bc.

--- libs/gui/SurfaceTexture.cpp
+++ libs/gui/SurfaceTexture.cpp
@@ -116,7 +116,7 @@ static int32_t createProcessUniqueId() {
 }
 
 SurfaceTexture::SurfaceTexture(GLuint tex, bool allowSynchronousMode,
-        GLenum texTarget, bool useFenceSync, const sp<BufferQueue> &bufferQueue) :
+        GLenum texTarget, bool useFenceSync) :
     mDefaultWidth(1),
     mDefaultHeight(1),
     mPixelFormat(PIXEL_FORMAT_RGBA_8888),
