cdb969910bb20c82cc3ed9515cde3b901e810984
Jonathan Bither <jonbither@gmail.com>
2012-05-23 09:36:53 -0400

    Revert "Fixup some missing OMX stuff copied from tpruvot"
    This reverts commit 6aa22b1c251cd256c0e4f988606290cd74ea3c22.
    
    Conflicts:
    
    	media/libstagefright/OMXCodec.cpp
    
    	modified:   media/libstagefright/OMXCodec.cpp

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -369,19 +369,6 @@ uint32_t OMXCodec::getComponentQuirks(
         quirks |= kRequiresAllocateBufferOnOutputPorts;
         quirks |= kDefersOutputBufferAllocation;
     }
-    if (!strcmp(componentName, "OMX.TI.Video.Decoder") ||
-            !strcmp(componentName, "OMX.TI.720P.Decoder")) {
-        // TI Video Decoder and TI 720p Decoder must use buffers allocated
-        // by Overlay for output port. So, I cannot call OMX_AllocateBuffer
-        // on output port. I must use OMX_UseBuffer on input port to ensure
-        // 128 byte alignment.
-        quirks |= kRequiresAllocateBufferOnInputPorts;
-        quirks |= kInputBufferSizesAreBogus;
-
-        if(kPreferThumbnailMode) {
-                quirks |= OMXCodec::kRequiresAllocateBufferOnOutputPorts;
-        }
-    }
     if (!strcmp(componentName, "OMX.TI.DUCATI1.VIDEO.DECODER")) {
         quirks |= kRequiresAllocateBufferOnInputPorts;
         quirks |= kRequiresAllocateBufferOnOutputPorts;
@@ -711,19 +698,10 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
                 LOGE("Profile and/or level exceed the decoder's capabilities.");
                 return ERROR_UNSUPPORTED;
             }
-
-            int32_t width, height;
-            bool success = meta->findInt32(kKeyWidth, &width);
-            success = success && meta->findInt32(kKeyHeight, &height);
-            CHECK(success);
-            if (!strcmp(mComponentName, "OMX.TI.720P.Decoder")
-                && (profile == kAVCProfileBaseline && level <= 39)
-                && (width*height <= MAX_RESOLUTION)
-                && (width <= MAX_RESOLUTION_WIDTH && height <= MAX_RESOLUTION_HEIGHT ))
-            {
-                // Though this decoder can handle this profile/level,
-                // we prefer to use "OMX.TI.Video.Decoder" for
-                // Baseline Profile with level <=39 and sub 720p
+            if (!strcmp(mComponentName, "OMX.google.h264.decoder")
+                && (profile != kAVCProfileBaseline)) {
+                LOGE("%s does not support profiles > kAVCProfileBaseline", mComponentName);
+                // The profile is unsupported by the decoder
                 return ERROR_UNSUPPORTED;
             }
 
