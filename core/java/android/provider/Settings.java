ae43d942a369fdf74d803f87c23e6f9996ceed45
Jesus David <jesuelfather@hotmail.com>
2012-05-30 14:27:50 -0500

    Frameworks: tablet powerwidget management (1/2)
    Settings: http://review.cyanogenmod.com/#/c/16842/
    
    Adds basic powerwidget management for tablet devices
    Allows to setup up to 6 different toggles, or remove all of them
    
    patchset 2: remove whitespaces and tabs
    patchset 3: remove some other tabs left
    patchset 4: some other tabs were hidden
    patchset 5: *changed strings for consistency with phone powerwidget
                *added license header on some files
    patchset 6: remove WIP tag
    
    Change-Id: I2e643401aaac0146857abe8c2d2dbc652d28e666

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2230,6 +2230,13 @@ public final class Settings {
         public static final String WIDGET_BUTTONS = "expanded_widget_buttons";
 
         /**
+         * Widget Buttons to Use - Tablet
+         *
+         * @hide
+         */
+        public static final String WIDGET_BUTTONS_TABLET = "expanded_widget_buttons_tablet";
+
+        /**
          * Navigation controls to Use
          *
          * @hide
