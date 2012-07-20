07ed94e92d3968508b3d407b58b72406d1cdebe2
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-07-20 21:16:19 +0200

    Really optimize the bootanimation :)
    Change-Id: I9da8fd7e6a587b5895519dd0983ec9b8f676771b

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -161,7 +161,7 @@ status_t BootAnimation::initTexture(void* buffer, size_t len)
     codec->setDitherImage(false);
     if (codec) {
         codec->decode(&stream, &bitmap,
-                SkBitmap::kARGB_8888_Config,
+                SkBitmap::kRGB_565_Config,
                 SkImageDecoder::kDecodePixels_Mode);
         delete codec;
     }
