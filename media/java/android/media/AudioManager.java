168ff80d5b1e70455f6bc8d68496fe12ae18ccae
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-07-24 10:57:30 +0200

    Reduce FM Changes, not required
    Change-Id: I93801574f88070aee1878bf3994260f2cb4c4d00

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -218,7 +218,6 @@ public class AudioManager {
     public static final int STREAM_TTS = AudioSystem.STREAM_TTS;
     /** @hide The audio stream for FM Radio */
     public static final int STREAM_FM = AudioSystem.STREAM_FM;
-
     /** Number of audio streams */
     /**
      * @deprecated Use AudioSystem.getNumStreamTypes() instead
@@ -238,7 +237,7 @@ public class AudioManager {
         7,  // STREAM_SYSTEM_ENFORCED
         11, // STREAM_DTMF
         11, // STREAM_TTS
-        11  // STREAM_FM
+        7   // STREAM_FM
     };
 
     /**
